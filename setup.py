85e55bde486a7aaa6f59f229573c7fe89bbb15e6
Lionking <sap.vaishnav@gmail.com>
2024-06-24 08:07:37 +0530

    Everything is install and configure for streamlit project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Lionking',
+    author_email="sap.vaishnav@gmail.com",
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+    
+    
+)
